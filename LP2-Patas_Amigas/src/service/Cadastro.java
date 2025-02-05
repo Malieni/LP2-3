8855963b8d8e90a5dcc10bee96010209205d867e
Malieni <gui.souza.malieni@gmail.com>
2025-02-05 14:37:44 -0300

    temos que arrumar as entidades

--- LP2-Patas_Amigas/src/service/Cadastro.java
+++
@@ -1,6 +0,0 @@
-package service;
-
-public interface Cadastro<T> {
-    void cadastrar(T t);
-    void listar();
-}
--- LP2-Patas_Amigas/src/view/PatasAmigas.java
+++
@@ -1,7 +0,0 @@
-public class PatasAmigas {
-
-    public static void main(String[] args) {
-        //Menu da página
-        Menu.show();
-    }
-}
